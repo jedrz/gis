5db81842a4463cda5a79b20d2d3df7ee14ed94f1
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2015-11-07 23:52:59 +0100

    ScalaJS hello world

--- gis-projekt/js/src/main/scala/gis/Main.scala
+++ gis-projekt/js/src/main/scala/gis/Main.scala
@@ -1,7 +1,11 @@
+package gis
+
 import scala.scalajs.js
 
 object Main extends js.JSApp {
   def main(): Unit = {
-    println("Hello World ScalaJS")
+    val graph = new Graph().withVertex(1).withVertex(2)
+    val vertices = graph.vertices
+    println(s"Hello World ScalaJS. Vertices = $vertices")
   }
 }
