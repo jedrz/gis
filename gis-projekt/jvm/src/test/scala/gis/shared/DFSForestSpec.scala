b8e6ce381a6bc0f37424e51ddc9248c3a926c8ae
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2015-11-14 21:14:22 +0100

    DFS forest

---
+++ gis-projekt/jvm/src/test/scala/gis/shared/DFSForestSpec.scala
@@ -0,0 +1,34 @@
+package gis.shared
+
+import gis.shared.DFSForest.toDFSForest
+
+class DFSForestSpec extends UnitSpec {
+
+  it should "return tree for connected graph" in {
+    val graph = Graphs.initGraphWithVertices(1 to 3).withEdge(1, 2).withEdge(2, 3).withEdge(1, 3)
+
+    val forest = graph.dfsForest.toList
+
+    forest should have size 1
+
+    val node1 = forest.head
+    node1.vertex should be (1)
+    node1.subtrees should have size 2
+
+    // Trochę słabe założenie, że tu będzie 3
+    val node3 = node1.subtrees(0)
+    node3.vertex should be (3)
+    node3.subtrees should be (empty)
+
+    val node2 = node1.subtrees(1)
+    node2.vertex should be (2)
+    node2.subtrees should be (empty)
+  }
+
+  it should "return forest for disconnected graph" in {
+    val graph = Graphs.initGraphWithVertices(1 to 4).withEdge(1, 2).withEdge(3, 4)
+
+    graph.dfsForest should have size 2
+  }
+
+}
---
+++ gis-projekt/shared/src/main/scala/gis/shared/DFSForest.scala
@@ -0,0 +1,54 @@
+package gis.shared
+
+case class Node(vertex: Vertex, subtrees: Stream[Node])
+
+class DFSForest(val graph: Graph) {
+
+  def dfsForest: Stream[Node] = {
+    val initialVisited = Set.empty[Vertex]
+    val initialForest = Stream.empty[Node]
+    graph.vertices.foldLeft((initialVisited, initialForest))((pair, vertex) => {
+      val (visited, forest) = pair
+      val treeOption = dfsTree(vertex, visited)
+      (visited ++ collectVisited(treeOption), forest #::: treeOption.toStream)
+    })._2
+  }
+
+  private def dfsTree(from: Vertex, visited: Set[Vertex] = Set.empty): Option[Node] = {
+    if (visited.contains(from)) {
+      None
+    } else {
+      dfsTreeForNotVisited(from, visited)
+    }
+  }
+
+  private def dfsTreeForNotVisited(from: Vertex, visited: Set[Vertex]): Option[Node] = {
+    val visitedWithFrom = visited + from
+    val adjacentNotVisited = graph.adjacencyLists(from).filterNot(visitedWithFrom)
+    val initialSubtrees = Stream.empty[Node]
+    val subtrees = adjacentNotVisited
+      .foldLeft((visited, initialSubtrees))((pair, adjVertex) => {
+        val (visited, subtrees) = pair
+        val dfsForAdj = dfsTree(adjVertex, visited)
+        (visited ++ collectVisited(dfsForAdj), subtrees #::: dfsForAdj.toStream)
+      }
+    )
+    Some(Node(from, subtrees._2))
+  }
+
+  private def collectVisited(node: Option[Node]): Set[Vertex] = {
+    node
+      .map { case Node(vertex, subtrees) =>
+          vertex #:: subtrees.flatMap(subtree => collectVisited(Some(subtree)))
+      }
+      .getOrElse(Set.empty)
+      .toSet
+  }
+
+}
+
+object DFSForest {
+  implicit def toDFSForest(graph: Graph): DFSForest = {
+    new DFSForest(graph)
+  }
+}
