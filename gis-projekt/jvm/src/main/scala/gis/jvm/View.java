d3821d835e381ab0e2a3df0f1e074f3ac32aec90
Igor Rodzik <igor.rodzik@gmail.com>
2015-11-15 17:13:47 +0100

    Add generated graphs visualization

--- gis-projekt/jvm/src/main/scala/gis/jvm/View.java
+++ gis-projekt/jvm/src/main/scala/gis/jvm/View.java
@@ -1,34 +1,71 @@
 package gis.jvm;
 
+import gis.shared.Generators;
 import gis.shared.Graph;
 import gis.shared.Graphs;
 import gis.shared.algorithms.other.LazyDFSBasedGraphConnectivity;
+import org.scalacheck.Gen;
+import scala.Tuple2;
+import scala.collection.immutable.List;
 import scala.runtime.AbstractFunction1;
 
 import javax.swing.*;
 import java.util.HashMap;
 import java.util.Map;
 
+import static gis.jvm.View.GraphType.CONNECTED;
+import static gis.jvm.View.GraphType.DISCONNECTED;
+
 public class View extends JFrame {
-    private JPanel panel1;
+    private JPanel mainPanel;
     private JButton nextStepButton;
-    private JComboBox<String> comboBox1;
+    private JComboBox<String> predefinedComboBox;
     private JPanel graphPanel;
+    private JButton generateDisconnectedButton;
+    private JButton generateConnectedButton;
+    private JTextField verticesNumTextField;
+    private JProgressBar algorithmProgressBar;
 
     GraphVisualization graphVisualization = new GraphVisualization(graphPanel);
 
     public View() {
         super("Graph Visualization");
-        this.setContentPane(panel1);
+        this.setContentPane(mainPanel);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setSize(1200, 800);
-        setUpComboBox(comboBox1);
+        setUpPredefinedComboBox(predefinedComboBox);
         this.setVisible(true);
 
-        nextStepButton.addActionListener((e) -> graphVisualization.nextStep());
+        nextStepButton.addActionListener((e) -> {
+            algorithmProgressBar.setValue(algorithmProgressBar.getValue() + 1);
+            graphVisualization.nextStep();
+        });
+        generateDisconnectedButton.addActionListener(e -> generateGraph(Integer.parseInt(verticesNumTextField.getText()), DISCONNECTED));
+        generateConnectedButton.addActionListener(e -> generateGraph(Integer.parseInt(verticesNumTextField.getText()), CONNECTED));
     }
 
-    public void setUpComboBox(JComboBox<String> comboBox) {
+    private void generateGraph(int verticesNum, GraphType graphType) {
+        Graph graph = getGeneratedGraph(verticesNum, graphType);
+        graphVisualization = new GraphVisualization(graphPanel);
+        Tuple2<List<Object>, Object> solution = new LazyDFSBasedGraphConnectivity(graph).solve();
+        algorithmProgressBar.setValue(0);
+        algorithmProgressBar.setMaximum(solution._1().size());
+        graphVisualization.visualize(solution, graph);
+        SwingUtilities.updateComponentTreeUI(this);
+    }
+
+    private Graph getGeneratedGraph(final int verticesNum, GraphType graphType) {
+        Gen<Graph> graphGen = graphType == DISCONNECTED ? Generators.notPartiallyConnected() : Generators.partiallyConnected();
+        return graphGen.suchThat(new AbstractFunction1<Graph, Object>() {
+            @Override
+            public Object apply(Graph v1) {
+                return v1.vertices().size() < verticesNum;
+            }
+        }).sample().get();
+    }
+
+
+    private void setUpPredefinedComboBox(JComboBox<String> comboBox) {
         Map<String, Graph> graphMap = new HashMap<>();
         Graphs.partiallyConnectedGraphs().foreach(new AbstractFunction1<Graph, String>() {
             @Override
@@ -52,8 +89,16 @@ public class View extends JFrame {
             graphVisualization = new GraphVisualization(graphPanel);
             String selectedItem = (String) comboBox.getSelectedItem();
             Graph selectedGraph = graphMap.get(selectedItem);
-            graphVisualization.visualize(new LazyDFSBasedGraphConnectivity(selectedGraph).solve(), selectedGraph);
+            Tuple2<List<Object>, Object> solution = new LazyDFSBasedGraphConnectivity(selectedGraph).solve();
+            algorithmProgressBar.setValue(0);
+            algorithmProgressBar.setMaximum(solution._1().size());
+            graphVisualization.visualize(solution, selectedGraph);
             SwingUtilities.updateComponentTreeUI(this);
         });
+    }
+
+    public enum GraphType {
+        DISCONNECTED,
+        CONNECTED
     }
 }
