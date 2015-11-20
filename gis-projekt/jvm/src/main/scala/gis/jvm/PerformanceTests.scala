a09f14035e90e4059ef60aa1af3f9715e30a949d
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2015-11-20 23:44:38 +0100

    Add class with performance tests

---
+++ gis-projekt/jvm/src/main/scala/gis/jvm/PerformanceTests.scala
@@ -0,0 +1,36 @@
+package gis.jvm
+
+import gis.shared.Graphs
+import gis.shared.algorithms.{MultipleDFSBasedAlgorithm, SCCBasedAlgorithm, SimpleAlgorithm}
+
+object PerformanceTests extends Bench.LocalTime {
+
+  override def defaultConfig: Context = super.defaultConfig ++ Context(
+    exec.minWarmupRuns -> 1,
+    exec.maxWarmupRuns -> 5,
+    exec.benchRuns -> 5
+  )
+
+  val sizes = Gen.range("size")(50, 300, 50)
+
+  val completeGraphs = sizes.map(Graphs.complete)
+
+  performance of "multiple dfs based algorithm" in {
+    using(completeGraphs) in { graph =>
+      new MultipleDFSBasedAlgorithm(graph).isPartiallyConnected
+    }
+  }
+
+  performance of "scc based algorithm" in {
+    using(completeGraphs) in { graph =>
+      new SCCBasedAlgorithm(graph).isPartiallyConnected
+    }
+  }
+
+  performance of "simple algorithm" in {
+    using(Gen.range("simple size")(10, 50, 10).map(Graphs.complete)) in { graph =>
+      new SimpleAlgorithm(graph).isPartiallyConnected
+    }
+  }
+
+}
